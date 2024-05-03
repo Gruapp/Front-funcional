c41b4ef9e83c70e4ce6bbb5495636f6296de5eb4
Gruapp <162225608+Gruapp@users.noreply.github.com>
2024-05-03 12:41:09 -0500

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,4 @@
 export const SERVER_PROTOCOL = "http"; // https
-//export const SERVER_DOMAIN = "localhost"; 
 export const SERVER_DOMAIN = "backfuncional-8bo6.onrender.com";
 export const SERVER_PORT = "3000";
 export const SERVER_URL = `${SERVER_PROTOCOL}://${SERVER_DOMAIN}${
