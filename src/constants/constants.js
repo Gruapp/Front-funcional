e9d5b2f84e6e1c9d06ec0a3c573bb2a3aa13a8c4
Gruapp <162225608+Gruapp@users.noreply.github.com>
2024-05-03 10:38:04 -0500

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-//export const SERVER_PROTOCOL = "http"; // https
+export const SERVER_PROTOCOL = "http"; // https
 export const SERVER_DOMAIN = "backfuncional-8bo6.onrender.com"; // backdesgruapchat.onrender.com
-//export const SERVER_PORT = "3000";
-export const SERVER_URL = backfuncional-8bo6.onrender.com;
+export const SERVER_PORT = "3000";
+export const SERVER_URL = "backfuncional-8bo6.onrender.com";
